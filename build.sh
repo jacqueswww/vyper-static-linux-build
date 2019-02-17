90aed329fa3b93de1cae5cade04b4497f6247c3f
Jacques Wagener <jacques@dilectum.co.za>
2019-02-18 00:25:56 +0200

    Trail it.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+CC="/usr/bin/musl-gcc"
 python="/usr/bin/python3"
 REPO_DIR="./vyper"
 OUTPUT_DIR="./bin"
@@ -31,30 +32,30 @@ function make_virtualenv {
 
 # Setup vyper
 make_virtualenv "$VYPER_VIRTUAL_ENV"
-make_virtualenv "$STATICX_VIRTUAL_ENV"
+# make_virtualenv "$STATICX_VIRTUAL_ENV"
 
-cd $REPO_DIR
+# pwd
 source $VYPER_VIRTUAL_ENV/bin/activate
-pip install -e .
 pip install pyinstaller
-cd ../
 
-source $STATICX_VIRTUAL_ENV/bin/activate
-pip install staticx
+# source $STATICX_VIRTUAL_ENV/bin/activate
+# pip install staticx
 
 # Build tags
 for tag in ${tags[@]}
 do
     rm -rf $REPO_DIR/dist
-    cd $REPO_DIR
-    source $VYPER_VIRTUAL_ENV/bin/activate
     echo -e "\n* ${GREEN} Compiling:${NC} $tag\n"
+    source $VYPER_VIRTUAL_ENV/bin/activate
+    cd $REPO_DIR
     git checkout tags/$tag
-    pyinstaller -y --onefile bin/vyper
+    pip uninstall vyper
+    pip install .
     cd ../
-    deactivate
-    source $STATICX_VIRTUAL_ENV/bin/activate
-    staticx $REPO_DIR/dist/vyper "${OUTPUT_DIR}/vyper-${tag}-"$(uname -m)
-    deactivate
-    exit
+    pyinstaller -y --onefile $REPO_DIR/bin/vyper
+    cp dist/vyper "${OUTPUT_DIR}/vyper-${tag}-"$(uname -m)
+
+    # source $STATICX_VIRTUAL_ENV/bin/activate
+    # staticx $REPO_DIR/dist/vyper "${OUTPUT_DIR}/vyper-${tag}-"$(uname -m)
+    # deactivate
 done
